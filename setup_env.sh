7b076e51b5680e7da4c41e91f5257685f2e113a6
Jacinto C <jachen36@gmail.com>
2021-08-03 21:32:51 -0700

    (feat) setup local install of code

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# make sure that pwd is this repo root.
 # TODO: convert to local venv within project
 # TODO: separete a develop and require file?
 conda create -n data-centric python=3.8 jupyterlab -y
 conda activate data-centric
 conda install keras -y
-conda install -c conda-forge pre-commit nb_black flake8 isort -y
+conda install -c conda-forge pre-commit nb_black flake8 isort pandas -y
+pip install --editable .
 
-# pre-commit install
+pre-commit install
